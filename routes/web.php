c03592a14830bbe099d8e3d2812e00a7b8a9a76c
nguyenngoctiengit <khocvole10c4@gmail.com>
2020-05-29 21:29:07 +0700

    dang nhap dang xuat trang admin

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,33 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+//Backend
+Route::get('/admin','AdminController@index');
+Route::get('/dashboard','AdminController@show_dashboard');
+Route::get('/logout','AdminController@logout');
+Route::post('/admin-dashboard','AdminController@dashboard');
+//Category Product
+Route::get('/add-category-product','CategoryProduct@add_category_product');
+Route::get('/edit-category-product/{IDLoai}','CategoryProduct@edit_category_product');
+Route::get('/delete-category-product/{IDLoai}','CategoryProduct@delete_category_product');
+Route::get('/all-category-product','CategoryProduct@all_category_product');
+Route::post('/save-category-product','CategoryProduct@save_category_product');
+Route::post('/update-category-product/{IDLoai}','CategoryProduct@update_category_product');
+
+//Brand Product
+Route::get('/add-brand-product','BrandProduct@add_brand_product');
+Route::get('/edit-brand-product/{brand_product_id}','BrandProduct@edit_brand_product');
+Route::get('/delete-brand-product/{brand_product_id}','BrandProduct@delete_brand_product');
+Route::get('/all-brand-product','BrandProduct@all_brand_product');
+Route::post('/save-brand-product','BrandProduct@save_brand_product');
+Route::post('/update-brand-product/{brand_product_id}','BrandProduct@update_brand_product');
+
+//Product
+Route::get('/add-product','ProductController@add_product');
+Route::get('/edit-product/{product_id}','ProductController@edit_product');
+Route::get('/delete-product/{product_id}','ProductController@delete_product');
+Route::get('/all-product','ProductController@all_product');
+Route::get('/unactive-product/{product_id}','ProductController@unactive_product');
+Route::get('/active-product/{product_id}','ProductController@active_product');
+Route::post('/save-product','ProductController@save_product');
+Route::post('/update-product/{product_id}','ProductController@update_product');
